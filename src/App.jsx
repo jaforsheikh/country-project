0adf0f3bad1cbe6312c60a8ea8b617b4e89ef779
jaforsheikh <jaforsheikh258@gmail.com>
2026-03-15 08:16:09 +0600

    I love my country added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 import Countries from './component/country/Countries'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-<Countries></Countries>
+    <h1>Bangladesh</h1>
+    <Countries></Countries>
 
     </>
   )
--- src/component/country/Countries.jsx
+++ src/component/country/Countries.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Countries = () => {
     return (
         <div>
-            <h1>The country <Bangladesh></Bangladesh></h1>
+            <h2>I love my country</h2>
         </div>
     );
 };
